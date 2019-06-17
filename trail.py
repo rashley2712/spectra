f91dfb878700e888d018f741c5110c2654939dcb
Richard Ashley <rashley@gmail.com>
2019-06-17 15:20:30 +0200

    trimming

--- trail.py
+++ trail.py
@@ -2,46 +2,83 @@
 import argparse, sys, numpy
 import matplotlib.pyplot
 import datetimelib
-import readmultispec
 import spectrumlib
 from astropy.io import fits
 
+class configClass():
+	def __init__(self):
+		self.lower = 0
+		self.upper = 1E6
+		self.order = 3
+		self.masklower = None
+		self.maskupper = None
+	
+	def load(self, filename="trail.cfg"):
+		try:
+			fileHandle = open(filename, 'rt')
+		except FileNotFoundError:
+			print("Could not open config file %s"%filename)
+			return
+		for line in fileHandle:
+			if line[0]=='#': continue
+			line = line.strip().split()
+			print(line)
+			try:
+				if line[0]=="wavelength":
+					self.lower = float(line[1])
+					self.upper = float(line[2])
+				if line[0]=="mask":
+					self.masklower = float(line[1])
+					self.maskupper = float(line[2])
+				if line[0]=="order":
+					self.order = int(line[1])
+			except IndexError:
+				continue
+			except Exception as e:
+				print(e)
+		fileHandle.close()
+		
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='Loads FITs spectrum file and plots it.')
-	parser.add_argument('inputFile', type=str, nargs="+", help='Name of the input file.')
-	parser.add_argument('-p', '--pause', type=float, default=1.0, help="Number of seconds to pause on each plot.")
+	parser = argparse.ArgumentParser(description='Draws a trailed spectrum from a list of spectra.')
+	parser.add_argument('inputFile', type=str, nargs="+", help='Names of the input file.')
+	
 	arg = parser.parse_args()
 
+	config = configClass()
+	config.load()
+	
 	plotWidth = 8
 	plotHeight = plotWidth/1.62
 	spectrumPlot = matplotlib.pyplot.figure(figsize=(plotWidth, plotHeight))
 
+	spectra = []
+
 	for index, filename in enumerate(arg.inputFile):
-		spectrum = readmultispec.readmultispec(filename)
-		hdul = fits.open(filename)
-		header = hdul[0]
-		specData = header.data
-		flux = specData[0][0]
-		errors = specData[3][0]
-		newSpectrum = spectrumlib.spectrumObject()
-		newSpectrum.loadedFromFilename = filename
-		newSpectrum.setData(spectrum['wavelen'], flux, errors)
-		newSpectrum.extractFITSHeaders(spectrum['header'])
-		outputFilename = newSpectrum.name + ".json"
-		newSpectrum.writeToJSON(outputFilename)
-	
+		spectrum = spectrumlib.spectrumObject()
+		spectrum.loadFromJSON(filename)
+		spectra.append(spectrum)
+		print("%d : %s"%(index+1, spectrum.name))
+	print("Loaded %d spectra."%len(spectra))
 
-		spectrumPlot.canvas.set_window_title(newSpectrum.name)
+	# Trim to the required wavelength range
+	for s in spectra:
+		s.trimWavelengthRange(config.lower, config.upper)
+
+	for spectrum in spectra:
+		spectrumPlot.canvas.set_window_title(spectrum.name)
 		
-		matplotlib.pyplot.step(spectrum['wavelen'], spectrum['flux'][0],  color = 'black')
+		matplotlib.pyplot.step(spectrum.wavelengths, spectrum.flux,  color = 'black')
+		fit = spectrum.fitPoly(config.order)
 
+		matplotlib.pyplot.plot(spectrum.wavelengths, fit, color='red', linestyle="--")
 	
 		matplotlib.pyplot.draw()
 		matplotlib.pyplot.show(block=False)
-		matplotlib.pyplot.pause(arg.pause)
+		matplotlib.pyplot.pause(0.5)
 
-		# input("Press enter to continue...")
+		key = input("Press enter to continue...")
+		if key=='q': sys.exit()
 		spectrumPlot.clf()
-		print(" ")
-
+	
